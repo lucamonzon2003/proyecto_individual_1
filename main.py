8a8fe4b234b02f6c491acb9caf47b7c8fc303bae
lucamonzon2003 <lucamonzon2003@gmail.com>
2024-03-18 23:27:51 -0300

    Fix X Deploy

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from class_functions import func_x_endpoints
 
 app = FastAPI()
-class_functions = func_x_endpoints()
 
 @app.get("/{num}")
 async def read_root(num : int):
@@ -10,20 +9,25 @@ async def read_root(num : int):
 
 @app.get("/PTG/genero/{genero}")
 def PlayTimeGenre (genero):
+    class_functions = func_x_endpoints()
     return class_functions.PlayTimeGenre(genero)
 
 @app.get("/UFG/genero/{genero}")
 def UserForGenre (genero):
+    class_functions = func_x_endpoints()
     return class_functions.UserForGenre(genero)
 
 @app.get("/UR/anio/{anio}")
 def UsersRecommend (anio : int):
+    class_functions = func_x_endpoints()
     return class_functions.UsersRecommend(anio)
 
 @app.get("/UNR/anio/{anio}")
 def UsersNotRecommend (anio : int):
+    class_functions = func_x_endpoints()
     return class_functions.UsersNotRecommend(anio)
 
 @app.get("/SA/anio/{anio}")
 def sentiment_analysis (anio : int):
+    class_functions = func_x_endpoints()
     return class_functions.sentiment_analysis(anio)
