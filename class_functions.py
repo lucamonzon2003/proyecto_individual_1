91f2d04225974d16b2d8e8f364238f9cfbcc7dd8
lucamonzon2003 <lucamonzon2003@gmail.com>
2024-03-18 23:55:06 -0300

    Fix X Deploy

--- class_functions.py
+++ class_functions.py
@@ -2,17 +2,11 @@ import pandas as pd
 import numpy as np
 
 class func_x_endpoints:
-    def __init__(self) -> None:
-        self.user_items_df = pd.DataFrame(pd.read_parquet("./data_transformed/user_items.parquet"))
-        self.reviews_df = pd.DataFrame(pd.read_parquet("./data_transformed/reviews.parquet"))
-        self.games_df = pd.DataFrame(pd.read_parquet("./data_transformed/games.parquet"))
-
     def PlayTimeGenre(self, genero: str):
         # Filtrar juegos por género
-        games_gnr = self.games_df.explode('genres')
+        games_gnr = pd.DataFrame(pd.read_parquet("./data_transformed/games.parquet")).explode('genres')
         games_gnr = games_gnr[games_gnr['genres'] == genero]
-
-        res = pd.merge(self.user_items_df, games_gnr, left_on='item_name', right_on='app_name', how='inner')
+        res = pd.merge(pd.DataFrame(pd.read_parquet("./data_transformed/user_items.parquet")), games_gnr, left_on='item_name', right_on='app_name', how='inner')
         res = res[res['release_date'] != 'Unknown']
         res['release_date'] = pd.to_datetime(res['release_date'])
         res_group = res[['release_date', 'playtime_forever']]
@@ -21,12 +15,12 @@ class func_x_endpoints:
         return {f"Año de lanzamiento con más horas jugadas para el Género {genero}": res_group['release_date'].iloc[0].year}
     def UserForGenre(self, genero: str):
         # Explorar los géneros para obtener un DataFrame con cada juego repetido por cada género al que pertenece
-        games_gnr = self.games_df.explode('genres')
+        games_gnr = pd.DataFrame(pd.read_parquet("./data_transformed/games.parquet")).explode('genres')
         # Filtrar los juegos por el género específico proporcionado como argumento
         games_gnr = games_gnr[games_gnr['genres'] == genero]
         
         # Fusionar los DataFrames de los elementos de usuario y juegos basados en el nombre del juego y la aplicación
-        games_gnr = pd.merge(self.user_items_df, games_gnr, left_on='item_name', right_on='app_name', how='inner')
+        games_gnr = pd.merge(pd.DataFrame(pd.read_parquet("./data_transformed/user_items.parquet")), games_gnr, left_on='item_name', right_on='app_name', how='inner')
         
         # Filtrar las fechas de lanzamiento desconocidas y convertirlas al tipo de datos datetime
         games_gnr = games_gnr[games_gnr['release_date'] != 'Unknown']
@@ -57,28 +51,31 @@ class func_x_endpoints:
         # Devolver un diccionario con el usuario con más horas jugadas para el género dado y las horas jugadas por año
         return  {f'Usuario con más horas jugadas para Género X {genero}': user_rank['user_id'], 'Horas jugadas': list_dicc}
     def UsersRecommend(self, año: int):
-        reviews = self.reviews_df[self.reviews_df['posted'].dt.year == año]
+        reviews = pd.DataFrame(pd.read_parquet("./data_transformed/reviews.parquet"))
+        reviews = reviews[reviews['posted'].dt.year == año]
         cond = (reviews['recommend'] == True) & (reviews['feeling'] >= 1)
         reviews = reviews[cond]
         rank_games = reviews['item_id'].value_counts().iloc[0:3].to_frame()
-        rank_games = pd.merge(rank_games, self.user_items_df, on='item_id', how='left')
+        rank_games = pd.merge(rank_games, pd.DataFrame(pd.read_parquet("./data_transformed/user_items.parquet")), on='item_id', how='left')
         rank_games = rank_games['item_name'].unique()
         res = [{"Puesto " + str(i+1): valor} for i, valor in enumerate(rank_games)]
         return res
     def UsersNotRecommend(self, año: int):
-        reviews = self.reviews_df[self.reviews_df['posted'].dt.year == año]
+        reviews = pd.DataFrame(pd.read_parquet("./data_transformed/reviews.parquet"))
+        reviews = reviews[reviews['posted'].dt.year == año]
         cond = (reviews['recommend'] == False) & (reviews['feeling'] == 0)
         reviews = reviews[cond]
         rank_games = reviews['item_id'].value_counts().iloc[0:3].to_frame()
-        rank_games = pd.merge(rank_games, self.user_items_df, on='item_id', how='left')
+        rank_games = pd.merge(rank_games, pd.DataFrame(pd.read_parquet("./data_transformed/user_items.parquet")), on='item_id', how='left')
         rank_games = rank_games['item_name'].unique()
         res = [{"Puesto " + str(i+1): valor} for i, valor in enumerate(rank_games)]
         return res
     def sentiment_analysis(self, año: int):
-        games = self.games_df[self.games_df['release_date'] != 'Unknown']
+        games = pd.DataFrame(pd.read_parquet("./data_transformed/games.parquet"))
+        games = games[games['release_date'] != 'Unknown']
         games = games[pd.to_datetime(games['release_date']).dt.year == año]
-        games_plus = pd.merge(games, self.user_items_df, left_on='app_name', right_on='item_name', how='left')
-        games_plus = pd.merge(games_plus, self.reviews_df, on='item_id', how='left')
+        games_plus = pd.merge(games, pd.DataFrame(pd.read_parquet("./data_transformed/user_items.parquet")), left_on='app_name', right_on='item_name', how='left')
+        games_plus = pd.merge(games_plus, pd.DataFrame(pd.read_parquet("./data_transformed/reviews.parquet")), on='item_id', how='left')
         count_dict = games_plus['feeling'].value_counts().to_dict()
         count_dict = {
             'Negative': count_dict.get(0),
@@ -86,5 +83,3 @@ class func_x_endpoints:
             'Positive': count_dict.get(2)
         }
         return count_dict
-    
-    
