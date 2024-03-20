d896064d6e7af0c2b78a354bc4589c128fee8c05
lucamonzon2003 <lucamonzon2003@gmail.com>
2024-03-20 04:09:16 -0300

    Fix X Deploy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 from class_functions import func_x_endpoints
 
 app = FastAPI()
@@ -9,20 +10,50 @@ async def read_root(num : int):
 
 @app.get("/PTG/genero/{genero}")
 def PlayTimeGenre (genero):
-    return func_x_endpoints.PlayTimeGenre(genero)
+    try:
+        result = func_x_endpoints.PlayTimeGenre(genero)
+        # Convertir el resultado a JSON
+        return result
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la serialización
+        raise HTTPException(status_code=500, detail="Error al procesar la solicitud")
 
 @app.get("/UFG/genero/{genero}")
 def UserForGenre (genero):
-    return func_x_endpoints.UserForGenre(genero)
+    try:
+        result = func_x_endpoints.UserForGenre(genero)
+        # Convertir el resultado a JSON
+        return result
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la serialización
+        raise HTTPException(status_code=500, detail="Error al procesar la solicitud")
 
 @app.get("/UR/anio/{anio}")
 def UsersRecommend (anio : int):
-    return func_x_endpoints.UsersRecommend(anio)
+    try:
+        result = func_x_endpoints.UsersRecommend(anio)
+        # Convertir el resultado a JSON
+        return result
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la serialización
+        raise HTTPException(status_code=500, detail="Error al procesar la solicitud")
 
 @app.get("/UNR/anio/{anio}")
 def UsersNotRecommend (anio : int):
-    return func_x_endpoints.UsersNotRecommend(anio)
-
+    try:
+        result = func_x_endpoints.UsersNotRecommend(anio)
+        # Convertir el resultado a JSON
+        return result
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la serialización
+        raise HTTPException(status_code=500, detail="Error al procesar la solicitud")
+    
 @app.get("/SA/anio/{anio}")
 def sentiment_analysis (anio : int):
-    return func_x_endpoints.sentiment_analysis(anio)
+    try:
+        result = func_x_endpoints.sentiment_analysis(anio)
+        # Convertir el resultado a JSON
+        return result
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la serialización
+        raise HTTPException(status_code=500, detail="Error al procesar la solicitud")
