11edb3bf0098120f25d574520819a0cbb6078762
lucamonzon2003 <lucamonzon2003@gmail.com>
2024-03-20 08:10:07 -0300

    Fix X Deploy

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from class_functions import func_x_endpoints
+import pandas as pd
 
 app = FastAPI()
+
+df_recomendacion_juego = pd.read_csv('df_recomendacion_juego.csv', header=0)
 
 @app.get("/{num}")
 async def read_root(num : int):
@@ -57,3 +60,13 @@ def sentiment_analysis (anio : int):
     except Exception as e:
         # Manejar cualquier excepción que pueda ocurrir durante la serialización
         raise HTTPException(status_code=500, detail="Error al procesar la solicitud")
+
+@app.get()
+def game_recommended(id: int):
+    df_id_juego = df_recomendacion_juego[df_recomendacion_juego['id_juego'] == id_juego]
+    if df_id_juego.empty:
+        return {"No se encontraron datos para el id {}".format(id_juego)}
+    top = df_id_juego.head(5)
+    nombre_juego = df_id_juego['nombre_juego'].iloc[0]
+    juegos_recomendados = {'Juegos recomendados para el juego {}:'.format(nombre_juego): set(top['juego'])}
+    return juegos_recomendados
